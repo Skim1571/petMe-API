a7a5b104f7b8a99813b5d8b58ff3599df4196810
Skim1571 <skim1571@gmail.com>
2022-09-02 10:34:55 -0500

    created relationships and updated admin.py

--- petMe_project/petMe/models.py
+++ petMe_project/petMe/models.py
@@ -10,12 +10,14 @@ class User(models.Model):
     return self.name
 
 class Species(models.Model):
-  species = models.CharField(max_length=50)
+  name = models.CharField(max_length=50)
 
   def __str__(self):
     return self.name
 
 class Pet(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pets', null=True)
+  species = models.ForeignKey(Species, on_delete=models.CASCADE, related_name='pets', unique=True, default=1)
   name = models.CharField(max_length=20)
   age = models.IntegerField()
   birth_date = models.DateField()
