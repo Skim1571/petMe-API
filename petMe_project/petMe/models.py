af833017e11335b1cac927868ef4716a61c034c3
Skim1571 <skim1571@gmail.com>
2022-09-02 10:29:58 -0500

    created 3 base models

--- petMe_project/petMe/models.py
+++ petMe_project/petMe/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  username = models.CharField(max_length=20)
+  email = models.CharField(max_length=50)
+  password = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+class Species(models.Model):
+  species = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+
+class Pet(models.Model):
+  name = models.CharField(max_length=20)
+  age = models.IntegerField()
+  birth_date = models.DateField()
+  hunger = models.IntegerField()
+  affection = models.IntegerField()
+  health = models.IntegerField()
+  last_play_time = models.DateTimeField()
+
+  def __str__(self):
+    return self.name
