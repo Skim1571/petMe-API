5f9724b2dbf553afb66f9306e57f6c1375e80121
Skim1571 <skim1571@gmail.com>
2022-09-13 16:05:25 -0500

    heroku updates in settings for static and whitenoise

--- petMe_project/petMe_django/settings.py
+++ petMe_project/petMe_django/settings.py
@@ -187,7 +187,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 WHITENOISE_USE_FINDERS = True
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR, "static/")
 
